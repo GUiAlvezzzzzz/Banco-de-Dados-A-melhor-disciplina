226ab730a45be5a6d189a0c55b7b4f2744bf0ab4
GUiAlvezzzzzz <143121343+GUiAlvezzzzzz@users.noreply.github.com>
2023-09-25 21:55:33 -0300

    Update ListaExercicios02.sql
    4. Verificação de Livros por Categoria - commit 04

--- ListaExercicios02.sql
+++ ListaExercicios02.sql
@@ -1,10 +1,18 @@
-CREATE PROCEDURE sp_ContarLivrosPorCategoria
+CREATE PROCEDURE sp_VerificarLivrosCategoria
     @Categoria NVARCHAR(255)
 AS
 BEGIN
-    SELECT COUNT(Livro.Livro_ID) AS TotalLivros
-    FROM Livro
-    INNER JOIN CategoriaLivro ON Livro.Livro_ID = CategoriaLivro.Livro_ID
-    INNER JOIN Categoria ON CategoriaLivro.Categoria_ID = Categoria.Categoria_ID
-    WHERE Categoria.Nome = @Categoria;
-END;         
+    IF EXISTS (
+        SELECT 1
+        FROM Livro
+        INNER JOIN CategoriaLivro ON Livro.Livro_ID = CategoriaLivro.Livro_ID
+        INNER JOIN Categoria ON CategoriaLivro.Categoria_ID = Categoria.Categoria_ID
+        WHERE Categoria.Nome = @Categoria
+    )
+    BEGIN
+        PRINT 'A categoria possui livros.';
+    END
+    ELSE
+    BEGIN
+        PRINT 'A categoria não possui livros.';
+    END;               
