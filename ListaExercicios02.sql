dd27568153090dfa62699a30fe14790e096c190c
GUiAlvezzzzzz <143121343+GUiAlvezzzzzz@users.noreply.github.com>
2023-09-25 21:54:58 -0300

    Update ListaExercicios02.sql
    3. Contagem de Livros por Categoria - Commit 03

--- ListaExercicios02.sql
+++ ListaExercicios02.sql
@@ -1,10 +1,10 @@
-CREATE PROCEDURE sp_LivrosPorCategoria
+CREATE PROCEDURE sp_ContarLivrosPorCategoria
     @Categoria NVARCHAR(255)
 AS
 BEGIN
-    SELECT Livro.Titulo
+    SELECT COUNT(Livro.Livro_ID) AS TotalLivros
     FROM Livro
     INNER JOIN CategoriaLivro ON Livro.Livro_ID = CategoriaLivro.Livro_ID
     INNER JOIN Categoria ON CategoriaLivro.Categoria_ID = Categoria.Categoria_ID
     WHERE Categoria.Nome = @Categoria;
-END;              
+END;         
