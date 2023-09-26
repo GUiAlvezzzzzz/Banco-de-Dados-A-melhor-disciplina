5e4dbf9be70dff13a78da5123db8728d948877b3
GUiAlvezzzzzz <143121343+GUiAlvezzzzzz@users.noreply.github.com>
2023-09-25 22:00:30 -0300

    Update ListaExercicios02.sql
    8. Autor Mais Antigo - Commit 08

--- ListaExercicios02.sql
+++ ListaExercicios02.sql
@@ -1,15 +1,7 @@
-CREATE PROCEDURE sp_AdicionarLivro
-    @Titulo NVARCHAR(255),
-    @EditoraID INT,
-    @AnoPublicacao INT
+CREATE PROCEDURE sp_AutorMaisAntigo
 AS
 BEGIN
-    BEGIN TRY
-        INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao)
-        VALUES (@Titulo, @EditoraID, @AnoPublicacao);
-        PRINT 'Livro adicionado com sucesso.';
-    END TRY
-    BEGIN CATCH
-        PRINT 'Erro ao adicionar o livro. Certifique-se de que o título seja único.';
-    END CATCH;
-END;               
+    SELECT TOP 1 Nome, Sobrenome
+    FROM Autor
+    ORDER BY Data_Nascimento ASC;
+END;                                                                 
