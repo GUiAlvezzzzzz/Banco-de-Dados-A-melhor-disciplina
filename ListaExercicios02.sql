1dfcd14fd8716c55bcd015348dc84c09afc2b10a
GUiAlvezzzzzz <143121343+GUiAlvezzzzzz@users.noreply.github.com>
2023-09-25 22:13:24 -0300

    Update ListaExercicios02.sql
    2. Livros por Categoria - COMMIT 02

--- ListaExercicios02.sql
+++ ListaExercicios02.sql
@@ -164,3 +164,14 @@ BEGIN
     SELECT Nome, Sobrenome
     FROM Autor;
 END;      
+
+CREATE PROCEDURE sp_LivrosPorCategoria
+    @Categoria NVARCHAR(255)
+AS
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN CategoriaLivro ON Livro.Livro_ID = CategoriaLivro.Livro_ID
+    INNER JOIN Categoria ON CategoriaLivro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = @Categoria;
+END;            
