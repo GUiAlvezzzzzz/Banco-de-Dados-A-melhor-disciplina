f11079e5212d029156a1039986dd77b1a7a57a55
GUiAlvezzzzzz <143121343+GUiAlvezzzzzz@users.noreply.github.com>
2023-09-25 21:55:53 -0300

    Update ListaExercicios02.sql
    5. Listagem de Livros por Ano - Commit 05

--- ListaExercicios02.sql
+++ ListaExercicios02.sql
@@ -1,18 +1,8 @@
-CREATE PROCEDURE sp_VerificarLivrosCategoria
-    @Categoria NVARCHAR(255)
+CREATE PROCEDURE sp_LivrosAteAno
+    @AnoPublicacao INT
 AS
 BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM Livro
-        INNER JOIN CategoriaLivro ON Livro.Livro_ID = CategoriaLivro.Livro_ID
-        INNER JOIN Categoria ON CategoriaLivro.Categoria_ID = Categoria.Categoria_ID
-        WHERE Categoria.Nome = @Categoria
-    )
-    BEGIN
-        PRINT 'A categoria possui livros.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'A categoria não possui livros.';
-    END;               
+    SELECT Titulo
+    FROM Livro
+    WHERE Ano_Publicacao <= @AnoPublicacao;
+END;                       
