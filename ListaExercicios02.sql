b97b2fa372cc9abb61d68400b26d471c7021203e
GUiAlvezzzzzz <143121343+GUiAlvezzzzzz@users.noreply.github.com>
2023-09-25 22:20:27 -0300

    Update ListaExercicios02.sql
    7. Adição de Livro com Tratamento de Erros - COMMIT 07

--- ListaExercicios02.sql
+++ ListaExercicios02.sql
@@ -228,3 +228,18 @@ BEGIN
     WHERE Categoria.Nome = CategoriaNome;
 END;
 
+ CREATE PROCEDURE sp_AdicionarLivro
+    @Titulo NVARCHAR(255),
+    @EditoraID INT,
+    @AnoPublicacao INT
+AS
+BEGIN
+    BEGIN TRY
+        INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao)
+        VALUES (@Titulo, @EditoraID, @AnoPublicacao);
+        PRINT 'Livro adicionado com sucesso.';
+    END TRY
+    BEGIN CATCH
+        PRINT 'Erro ao adicionar o livro. Certifique-se de que o título seja único.';
+    END CATCH;
+END;                               
